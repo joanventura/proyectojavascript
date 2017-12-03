b64c5494842211199f233612fdbfda6d930dcfdb
Joan Ventura <ing.joanventura@gmail.com>
2017-12-03 12:49:27 -0400

    registro de estudiante con JSON y localstorage

--- resources/js/estudiantes.js
+++ resources/js/estudiantes.js
@@ -7,54 +7,73 @@ var Estudiante = function(){
 			self.telefono="";
 			self.email="";
 		};
-	
-		function salvar(){
-			var id = document.getElementById("id").value;
-			var nombre = document.getElementById("nombre").value;
-			var matricula = document.getElementById("matricula").value;
-			var identificacion = document.getElementById("identificacion").value;
-			var telefono = document.getElementById("telefono").value;
-			var email = document.getElementById("email").value;
 
-			
-			var est1= new Estudiante()
-			est1.id=id;
-			est1.nombre=nombre;
-			est1.matricula=matricula;
-			est1.identificacion=identificacion;
-			est1.telefono=telefono;
-			est1.email=email;
-			
-			var table=document.getElementById("estudiantes");
-			var tr=document.createElement("tr");
-			var tdId= document.createElement("td");
-			var tdNombre= document.createElement("td");
-			var tdMatricula= document.createElement("td");
-			var tdIdentificacion= document.createElement("td");
-			var tdTelefono= document.createElement("td");
-			var tdEmail= document.createElement("td");
-			
-			var txtId=document.createTextNode(id);
-			var txtNombre=document.createTextNode(nombre);
-			var txtMatricula=document.createTextNode(matricula);
-			var txtIdentificacion=document.createTextNode(identificacion);
-			var txtTelefono=document.createTextNode(telefono);
-			var txtEmail=document.createTextNode(email);
-			
-			
-			tdId.appendChild(txtId);
-			tr.appendChild(tdId);
-			tdNombre.appendChild(txtNombre);
-			tr.appendChild(tdNombre);
-			tdMatricula.appendChild(txtMatricula);
-			tr.appendChild(tdMatricula);
-			tdIdentificacion.appendChild(txtIdentificacion);
-			tr.appendChild(tdIdentificacion);
-			tdTelefono.appendChild(txtTelefono);
-			tr.appendChild(tdTelefono);
-			tdEmail.appendChild(txtEmail);
-			tr.appendChild(tdEmail);
-			
-			table.appendChild(tr);
-			
-		}
+function agregarEstudiante(estudiante){
+	var rowEstudiante = 
+		"<tr>"
+			+ "<td>" + estudiante.id + "</td>"
+			+ "<td>" + estudiante.nombre + "</td>"
+			+ "<td>" + estudiante.matricula + "</td>"
+			+ "<td>" + estudiante.identificacion + "</td>"
+			+ "<td>" + estudiante.telefono + "</td>"
+			+ "<td>" + estudiante.email + "</td>"
+			+ "<td>" + '<button id="borrar'+estudiante.id+'" name="borrar" class="btn btn-danger btn-xs">X</button>'+ "</td>"
+		+ "</tr>";
+		
+	$("table tbody").append(rowEstudiante);
+};
+
+function guardarDB(estudiante){
+	myStorage = window.localStorage;
+	
+	var estudiantes = [];
+	
+	var dbEstudiantes = myStorage.getItem('estudiantes');
+	if(dbEstudiantes!=null){
+		estudiantes = JSON.parse(dbEstudiantes);
+	}
+	
+	estudiantes.push(estudiante);
+	myStorage.setItem("estudiantes", JSON.stringify(estudiantes));
+};
+		
+$(document).ready(function(){
+	myStorage = window.localStorage;
+	
+	var dbEstudiantes = myStorage.getItem('estudiantes');
+	if(dbEstudiantes!=null){
+		estudiantes = JSON.parse(dbEstudiantes);
+		
+		$.each(estudiantes, function(i,est){
+			agregarEstudiante(est);
+		});
+	}
+	
+	$('#salvar').click(function(){
+		var id = $('#id').val();
+		var nombre = $('#nombre').val();
+		var matricula = $('#matricula').val();
+		var identificacion = $('#identificacion').val();
+		var telefono = $('#telefono').val();
+		var email = $('#email').val();
+		
+		var est= new Estudiante()
+		est.id=id;
+		est.nombre=nombre;
+		est.matricula=matricula;
+		est.identificacion=identificacion;
+		est.telefono=telefono;
+		est.email=email;
+		
+		agregarEstudiante(est);
+		guardarDB(est);
+	});
+	
+	$('#borrar').click(function() {
+	
+		var rowIndex = $("#estudiantes TR").index();
+
+		alert( rowIndex );  // alert the index number of the clicked row.
+
+	});
+});
